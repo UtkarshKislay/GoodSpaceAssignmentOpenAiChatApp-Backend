a2ef0170222fe6ca7a474010ab6c04bc6707efb4
Utkarsh Kislay <utkarsh2237@gmial.com>
2024-01-03 14:42:29 +0530

    initialize prev chat

--- controller/openAiController.js
+++ controller/openAiController.js
@@ -1,33 +1,51 @@
 const OpenAI = require("openai");
-const messageModel=require("../modal/Message");
+const messageModel = require("../modal/Message");
 class OpenAiChat {
+
+  static initializeChatResponseWithUser = async (req, res) => {
+    try {
+      const { email } = req.body;
+      const prevChat = await messageModel.find({
+        $or: [{ email: email, senderAdd: "User" }, { email: email, senderAdd: "openAi" }]
+      }).sort({createdAt:1});
+      console.log(prevChat);
+      return res.status(200).json({ message: "Initialize Chat", data: prevChat });
+
+    } catch (err) {
+      return res.status(500).json({ error: "An error occurred" });
+    }
+
+  }
+
+
+
   static chatResponse = async (req, res) => {
     try {
       const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
-      const {email, message } = req.body;
+      const { email, message } = req.body;
       console.log(email);
-      const messageModelStruct={
-        email:email,
-        senderAdd:"User",
-        message:message,
-        createdAt:new Date()
+      const messageModelStruct = {
+        email: email,
+        senderAdd: "User",
+        message: message,
+        createdAt: new Date().toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'})
       }
       await messageModel(messageModelStruct).save();
       const chatCompletion = await openai.chat.completions.create({
         messages: [{ role: "user", content: message }],
         model: "gpt-3.5-turbo",
       });
-      const messageRecieved=chatCompletion.choices[0].message["content"];
-      const messageModelStructByOpenAi={
-        email:email,
-        senderAdd:"openAi",
-        message:messageRecieved,
-        createdAt:new Date()
+      const messageRecieved = chatCompletion.choices[0].message["content"];
+      const messageModelStructByOpenAi = {
+        email: email,
+        senderAdd: "openAi",
+        message: messageRecieved,
+        createdAt: new Date().toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'})
       }
       await messageModel(messageModelStructByOpenAi).save();
-      return res.status(200).json({message:"Query execute Successfully",data:messageRecieved});
+      return res.status(200).json({ message: "Query execute Successfully", data: messageRecieved });
     } catch (error) {
- 
+
       return res.status(500).json({ error: "An error occurred" });
     }
   }
--- routes/OpenApi.js
+++ routes/OpenApi.js
@@ -5,6 +5,7 @@ const router=express.Router();
 
 const openAiChat =require('../controller/openAiController');
 
+router.post('/openai/initializeChatResponse',openAiChat.initializeChatResponseWithUser);
 router.post('/openai/chat',openAiChat.chatResponse);
 
 module.exports=router;
