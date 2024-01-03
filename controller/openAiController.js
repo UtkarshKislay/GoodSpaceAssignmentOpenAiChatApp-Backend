2bf9d02994123bd58ac7a0af51bb566ce01eede2
Utkarsh Kislay <utkarsh2237@gmial.com>
2024-01-03 14:10:32 +0530

    create message model

--- controller/openAiController.js
+++ controller/openAiController.js
@@ -1,20 +1,37 @@
 const OpenAI = require("openai");
+const messageModel=require("../modal/Message");
 class OpenAiChat {
   static chatResponse = async (req, res) => {
     try {
       const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
-      const { message } = req.body;
+      const {email, message } = req.body;
+      console.log(email);
+      const messageModelStruct={
+        email:email,
+        senderAdd:"User",
+        message:message,
+        createdAt:new Date()
+      }
+      await messageModel(messageModelStruct).save();
       const chatCompletion = await openai.chat.completions.create({
         messages: [{ role: "user", content: message }],
         model: "gpt-3.5-turbo",
       });
-      console.log(chatCompletion.choices[0].message["content"]);
-      return chatCompletion.choices[0].message["content"];
+      const messageRecieved=chatCompletion.choices[0].message["content"];
+      const messageModelStructByOpenAi={
+        email:email,
+        senderAdd:"openAi",
+        message:messageRecieved,
+        createdAt:new Date()
+      }
+      await messageModel(messageModelStructByOpenAi).save();
+      return res.status(200).json({message:"Query execute Successfully",data:messageRecieved});
     } catch (error) {
-      console.log(error);
+ 
       return res.status(500).json({ error: "An error occurred" });
     }
   }
 }
 
 module.exports = OpenAiChat;
+
