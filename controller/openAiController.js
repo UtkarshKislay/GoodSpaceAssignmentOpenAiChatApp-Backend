1c409448a813686329b2d0bed9342b76b3b0afda
Utkarsh Kislay <utkarsh2237@gmial.com>
2024-01-04 14:04:24 +0530

    update in message model

--- controller/openAiController.js
+++ controller/openAiController.js
@@ -1,5 +1,7 @@
 const OpenAI = require("openai");
 const messageModel = require("../modal/Message");
+const { v4: uuidv4 } = require('uuid');
+
 class OpenAiChat {
 
   static initializeChatResponseWithUser = async (req, res) => {
@@ -7,46 +9,56 @@ class OpenAiChat {
       const { email } = req.body;
       const prevChat = await messageModel.find({
         $or: [{ email: email, senderAdd: "User" }, { email: email, senderAdd: "openAi" }]
-      }).sort({createdAt:1});
+      })
+      .select('-_id uniqueId message senderAdd createdAt')
+      .sort({createdAt:1});
       console.log(prevChat);
       return res.status(200).json({ message: "Initialize Chat", data: prevChat });
 
     } catch (err) {
       return res.status(500).json({ error: "An error occurred" });
     }
-
   }
 
 
 
   static chatResponse = async (req, res) => {
     try {
-      const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY });
-      const { email, message } = req.body;
-      console.log(email);
+      const openai = new OpenAI({ apiKey: process.env.OPEN_API_KEY_OLD });
+      const { email, message,uniqueId,createdAt } = req.body;
+      console.log("email: ", email," ",message);
       const messageModelStruct = {
+        uniqueId:uniqueId,
         email: email,
         senderAdd: "User",
         message: message,
-        createdAt: new Date().toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'})
+        createdAt: createdAt
       }
+      console.log(messageModelStruct);
       await messageModel(messageModelStruct).save();
       const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: message }],
+        messages: [{ role: "user", content: message
+        //  +" "+process.env.SECRET_COMMAND
+         }],
         model: "gpt-3.5-turbo",
       });
       const messageRecieved = chatCompletion.choices[0].message["content"];
+      console.log(messageRecieved);
       const messageModelStructByOpenAi = {
+        uniqueId:uuidv4(),
         email: email,
         senderAdd: "openAi",
         message: messageRecieved,
         createdAt: new Date().toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'})
       }
       await messageModel(messageModelStructByOpenAi).save();
-      return res.status(200).json({ message: "Query execute Successfully", data: messageRecieved });
+      const dataToSend = { ...messageModelStructByOpenAi };
+      delete dataToSend.email;
+      console.log(dataToSend);
+      return res.status(200).json({ message: "Query execute Successfully", data: dataToSend });
     } catch (error) {
-
-      return res.status(500).json({ error: "An error occurred" });
+      console.log(error);
+      return res.status(500).json({ message: "An error occurred" });
     }
   }
 }
--- modal/Message.js
+++ modal/Message.js
@@ -1,6 +1,10 @@
 const mongoose =require('mongoose');
 
 const messageSchema=new mongoose.Schema({
+    uniqueId:{
+        type:String,
+        required:true,
+    },
     email:{
         type:String,
         required:true,
